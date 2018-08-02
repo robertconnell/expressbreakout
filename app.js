d0cf407d94e4c431e784026f4edd014cd4087523
Robert Connell <robertconnell@Kristinas-MacBook-Pro.local>
2018-08-02 11:13:17 -0600

    put parens around port variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.NODE_ENV || 7001
+const port = (process.env.NODE_ENV || 7001)
 const queries = require("./queries")
 
 app.get('/', (req, res, next) => {
